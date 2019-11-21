5c3da419c0c2a6d4ebe6100e6ab05d1371e8e7b7
fabianCor <50028283+fabianCor@users.noreply.github.com>
2019-11-20 22:25:34 -0800

    package sjPlot is not installing properly in most systems and crashes the .rmd we switch to 'stargazer' here. And add saveRDS() as a safety net for future debugging

--- Statistical_analysis.R
+++ Statistical_analysis.R
@@ -1,10 +1,7 @@
 # here we start analysis the HKANDLE data:
 
- # require(sjPlot)
- # require(sjlabelled)
- # require(sjmisc)
-# require(kableExtra)
-  require(lspline)
+
+require(stargazer)
 
 #raw_data_averages <- read.csv("/Users/fcorlier/Box/Fabian_ERM_Box/Khandle_data_analysis_FABIAN/R_KHANDLE_project_data/raw_data_averages.csv")[,2:694]#saving as csv always adds a column of indices in [,1] so I load from 2 to ncol+1
 ListOfVars <- c("semantic_memory","adj_verbal_episodic_mem","executive_function","Age_centered_75","Age_centered_75_decades","GENDER","race","CONCERNED_THINKING","yrEDUCATION_centered",Ecog_12items, "RELATIVE_DEMENTIA","SIBLING_DEMENTIA","PARENTAL_DEMENTIA","Ecog12_including_partial_averages","depression_01","logEcog12")
@@ -51,35 +48,62 @@ for(n in 1:5){
 memfit[[n+1]]<- lm(data = DFmem, formula = mem[[n]])
 }
 
-#creating a result table
-TwoVarMem <-tab_model(memfit, title = "Relation between log(ECog) and memory", 
-          auto.label = T,
-          dv.labels = c("Modifier: age","Modifier: Gender","Modifier: race/ethnicity","Modifier: Education","Modifier: Family hist.","Modifier: Depr. symptoms"),
-          digits = 3,
-          ci.hyphen = ", ",
-          string.ci = "95% CI",
-          show.p = F,
-          pred.labels =c("Intercept",
-                         "Memory",
-                         "Age (in decades, cetered at 75)",
-                         "Memory*Age interaction",
-                         "Male gender",
-                         "Memory*Gender interaction",
-                         "Asian", 
-                         "Black", 
-                         "Latino",
-                         "Memory*Asian interaction",
-                         "Memory*Black interaction",
-                         "Memory*Latino interaction",
-                         "Education (years, centered at 12)",
-                         "Memory*Education interaction",
-                         "Family history of dementia",
-                         "Memory*Family hist. interaction",
-                         "Depressive symptoms",
-                         "Memory*Depr. symptoms interaction"),
-          file = here("Rmarkdown_scripts_and_outputs","Formated_regression_tables","Formatted_model_results_memory.html"))
 
-TwoVarMem
+#using a simpler alternative because "sjPlot" has trouble loading consistently 
+episodic_memory_table <- stargazer(memfit,type = "text",
+          out = here("text_ouptput_memory.txt"),
+          ci = TRUE,digits = 3,
+          column.labels=c("Modifier: age","Modifier: Gender","Modifier: race/ethnicity","Modifier: Education","Modifier: Family hist.","Modifier: Depr. symptoms"),
+          covariate.labels=NULL,
+          no.space=TRUE,
+          notes="Regression estimates by modifier, models (2-6) are all controlled for age and additionally to age, model (3) controls for race and model (6) for Gender",
+          notes.append=FALSE,
+          report=c("vcs"))
+
+episodic_memory_table2 <- stargazer(memfit,type = "text",
+                                   out = here("text_ouptput_memory2.txt"),
+                                   ci = F,digits = 3,
+                                   column.labels=c("Modifier: age","Modifier: Gender","Modifier: race/ethnicity","Modifier: Education","Modifier: Family hist.","Modifier: Depr. symptoms"),
+                                   covariate.labels=NULL,
+                                   no.space=TRUE,
+                                   notes="Regression estimates by modifier, models (2-6) are all controlled for age and additionally to age, model (3) controls for race and model (6) for Gender",
+                                   notes.append=FALSE,
+                                   report=c("vcsp*"))
+
+episodic_memory_table
+episodic_memory_table2
+
+saveRDS(memfit,file = here("regression_fit_Memory.rds")) #this saves all the regression fits
+
+#creating a result table
+# TwoVarMem <-tab_model(memfit, title = "Relation between log(ECog) and memory", 
+#           auto.label = T,
+#           dv.labels = c("Modifier: age","Modifier: Gender","Modifier: race/ethnicity","Modifier: Education","Modifier: Family hist.","Modifier: Depr. symptoms"),
+#           digits = 3,
+#           ci.hyphen = ", ",
+#           string.ci = "95% CI",
+#           show.p = T,
+#           pred.labels =c("Intercept",
+#                          "Memory",
+#                          "Age (in decades, cetered at 75)",
+#                          "Memory*Age interaction",
+#                          "Male gender",
+#                          "Memory*Gender interaction",
+#                          "Asian", 
+#                          "Black", 
+#                          "Latino",
+#                          "Memory*Asian interaction",
+#                          "Memory*Black interaction",
+#                          "Memory*Latino interaction",
+#                          "Education (years, centered at 12)",
+#                          "Memory*Education interaction",
+#                          "Family history of dementia",
+#                          "Memory*Family hist. interaction",
+#                          "Depressive symptoms",
+#                          "Memory*Depr. symptoms interaction"),
+#           file = here("Rmarkdown_scripts_and_outputs","Formated_regression_tables","Formatted_model_results_memory.html"))
+# 
+# TwoVarMem
 
 
 # ## @knitr stepwise-model-memory  
@@ -122,43 +146,67 @@ for (i in 1:length(modlistex)) {
   results_execfun[[i+1]] <- model
 }
 
+#same as above simpler output
+executive_function_table <- stargazer(results_execfun,type = "text",
+          out = here("text_ouptput_exec_fun.txt"),
+          ci = TRUE,digits = 3,
+          column.labels=c("Modifier: age","Modifier: Gender","Modifier: race/ethnicity","Modifier: Education","Modifier: Family hist.","Modifier: Depr. symptoms"),
+          covariate.labels=NULL,
+          no.space=TRUE,
+          notes="Regression estimates by modifier, models (2-6) are all controlled for age and additionally to age, model (3) controls for race and model (6) for Gender",
+          notes.append=FALSE,
+          report=c("vcs"))
+
+executive_function_table2 <- stargazer(results_execfun,type = "text",
+                                      out = here("text_ouptput_exec_fun2.txt"),
+                                      ci = TRUE,digits = 3,
+                                      column.labels=c("Modifier: age","Modifier: Gender","Modifier: race/ethnicity","Modifier: Education","Modifier: Family hist.","Modifier: Depr. symptoms"),
+                                      covariate.labels=NULL,
+                                      no.space=TRUE,
+                                      notes="Regression estimates by modifier, models (2-6) are all controlled for age and additionally to age, model (3) controls for race and model (6) for Gender",
+                                      notes.append=FALSE,
+                                      report=c("vcsp*"))
+executive_function_table
+executive_function_table2
+saveRDS(results_execfun,file = here("regression_fit_EXFUN.rds")) #this saves all the regression fits
+
 #creating a result table
-TwoVarEx<-tab_model(results_execfun, title = "Relation between log(ECog) and executive function",
-          auto.label = T,
-          dv.labels = c("Modifier: AGE","Modifier: GENDER","Modifier: Race/Ethnicity","Modifier: Education", "Modifier: Family history","Modifier: depression"),
-          digits = 3,
-          ci.hyphen = ", ",
-          show.p = F,
-          string.ci = "95% CI",
-          pred.labels =c("Intercept",
-                         "Executive fun.(<1)",
-                         "Executive fun.(>1)",
-                         "Age (in decades, cetered at 75)",
-                         "Executive fun.(<1)*Age interaction",
-                         "Executive fun.(>1)*Age interaction",
-                         "Male gender",
-                         "Executive fun.(<1)*Gender interaction",
-                         "Executive fun.(>1)*Gender interaction",
-                         "Asian",
-                         "Black",
-                         "Latino",
-                         "Executive fun.(<1)*Asian interaction",
-                         "Executive fun.(>1)*Asian interaction",
-                         "Executive fun.(<1)*Black interaction",
-                         "Executive fun.(>1)*Black interaction",
-                         "Executive fun.(<1)*Latino interaction",
-                         "Executive fun.(>1)*Latino interaction",
-                         "Education (years, centered at 12)",
-                         "Executive fun.(<1)*Education interaction",
-                         "Executive fun.(>1)*Education interaction",
-                         "Family history of dementia",
-                         "Executive fun.(<1)*Family hist interaction",
-                         "Executive fun.(>1)*Family hist interaction",
-                         "Depressive symptoms",
-                         "Executive fun.(<1)*Depr. symptoms interaction",
-                         "Executive fun.(>1)*Depr. symptoms interaction"),
-          file = here("Rmarkdown_scripts_and_outputs","Formated_regression_tables","Formatted_model_results_executive.html"))
-TwoVarEx
+# TwoVarEx<-tab_model(results_execfun, title = "Relation between log(ECog) and executive function",
+#           auto.label = T,
+#           dv.labels = c("Modifier: AGE","Modifier: GENDER","Modifier: Race/Ethnicity","Modifier: Education", "Modifier: Family history","Modifier: depression"),
+#           digits = 3,
+#           ci.hyphen = ", ",
+#           show.p = F,
+#           string.ci = "95% CI",
+#           pred.labels =c("Intercept",
+#                          "Executive fun.(<1)",
+#                          "Executive fun.(>1)",
+#                          "Age (in decades, cetered at 75)",
+#                          "Executive fun.(<1)*Age interaction",
+#                          "Executive fun.(>1)*Age interaction",
+#                          "Male gender",
+#                          "Executive fun.(<1)*Gender interaction",
+#                          "Executive fun.(>1)*Gender interaction",
+#                          "Asian",
+#                          "Black",
+#                          "Latino",
+#                          "Executive fun.(<1)*Asian interaction",
+#                          "Executive fun.(>1)*Asian interaction",
+#                          "Executive fun.(<1)*Black interaction",
+#                          "Executive fun.(>1)*Black interaction",
+#                          "Executive fun.(<1)*Latino interaction",
+#                          "Executive fun.(>1)*Latino interaction",
+#                          "Education (years, centered at 12)",
+#                          "Executive fun.(<1)*Education interaction",
+#                          "Executive fun.(>1)*Education interaction",
+#                          "Family history of dementia",
+#                          "Executive fun.(<1)*Family hist interaction",
+#                          "Executive fun.(>1)*Family hist interaction",
+#                          "Depressive symptoms",
+#                          "Executive fun.(<1)*Depr. symptoms interaction",
+#                          "Executive fun.(>1)*Depr. symptoms interaction"),
+#           file = here("Rmarkdown_scripts_and_outputs","Formated_regression_tables","Formatted_model_results_executive.html"))
+# TwoVarEx
 
 
 
@@ -180,13 +228,13 @@ interactions_ex_nospline <- c(BASEex_nospline, "ex_function*race", "ex_function*
 
 results_execfun_nospline<-list()
 results_execfun_nospline[[1]] <- lm(BASEex_nospline,data = DFex)
-basetabex_nospline<-tab_model(results_execfun_nospline[[1]], title = "Relation between ECog and executive function", 
-                     auto.label = T,
-                     dv.labels = "Base model logEcog",
-                     digits = 3,
-                     ci.hyphen = ", ",
-                     file = here("Rmarkdown_scripts_and_outputs","Formated_regression_tables","Base_model_executive_nospline.html"))
-basetabex_nospline
+# basetabex_nospline<-tab_model(results_execfun_nospline[[1]], title = "Relation between ECog and executive function", 
+#                      auto.label = T,
+#                      dv.labels = "Base model logEcog",
+#                      digits = 3,
+#                      ci.hyphen = ", ",
+#                      file = here("Rmarkdown_scripts_and_outputs","Formated_regression_tables","Base_model_executive_nospline.html"))
+# basetabex_nospline
 
 modlistex_nospline <- list()
 for(t in 1:6){
@@ -200,13 +248,13 @@ for (i in 1:length(modlistex)) {
   model<- lm(data = DFex, formula = modlistex_nospline[[i]])
   results_execfun_nospline[[i+1]] <- model
 }
-TwoVarEx_NS<-tab_model(results_execfun_nospline, title = "Relation between ECog and executive function",
-                    auto.label = T,
-                    dv.labels = c("Base + Race/Ethnicity","Base + GENDER","Base + Education + race", "Base + Family history","Base + depression","Base + Education"),
-                    digits = 3,
-                    ci.hyphen = ", ",
-                    file = here("Rmarkdown_scripts_and_outputs","Formated_regression_tables","2variable_models_executive_nospline.html"))
-TwoVarEx_NS
+# TwoVarEx_NS<-tab_model(results_execfun_nospline, title = "Relation between ECog and executive function",
+#                     auto.label = T,
+#                     dv.labels = c("Base + Race/Ethnicity","Base + GENDER","Base + Education + race", "Base + Family history","Base + depression","Base + Education"),
+#                     digits = 3,
+#                     ci.hyphen = ", ",
+#                     file = here("Rmarkdown_scripts_and_outputs","Formated_regression_tables","2variable_models_executive_nospline.html"))
+# TwoVarEx_NS
 
 
 
